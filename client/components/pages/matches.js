aa18f7d779f279f08b09c5eb4bce917f58e19aed
Jon-Paul Glencross <jon-paul.glencross@uk.ibm.com>
2019-03-15 19:14:30 +0000

    Added card to matches page

--- client/components/pages/matches.js
+++ client/components/pages/matches.js
@@ -6,36 +6,45 @@ import TixnerLayout from "./TixnerLayout";
 import { Header } from "govuk-react";
 import Table from "@govuk-react/table";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const WhiteBackground = styled("div")`
+  background: white;
+  padding: 30px;
+  border-radius: 5px;
+`;
 
 const Matches = () => {
   return (
     <TixnerLayout>
-      <Header level={1} size="LARGE">
-        Matches
-      </Header>
-      <Table caption="Profile:">
-        <Table.Row>
-          <Table.CellHeader>Full Name:</Table.CellHeader>
-          <Table.Cell>Nick Casadei</Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.CellHeader>Service Line:</Table.CellHeader>
-          <Table.Cell>iX</Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.CellHeader>Time at IBM:</Table.CellHeader>
-          <Table.Cell>3 months to 1 year</Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.CellHeader>Focus Area:</Table.CellHeader>
-          <Table.Cell>Front-End</Table.Cell>
-        </Table.Row>
-      </Table>
-      <ThumbDownButton />
-      <Link to="/projects">
-        <ThumbUpButton />
-      </Link>
-      <ProfileImage />
+      <WhiteBackground>
+        <Header level={1} size="LARGE">
+          Matches
+        </Header>
+        <Table caption="Profile:">
+          <Table.Row>
+            <Table.CellHeader>Full Name:</Table.CellHeader>
+            <Table.Cell>Nick Casadei</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.CellHeader>Service Line:</Table.CellHeader>
+            <Table.Cell>iX</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.CellHeader>Time at IBM:</Table.CellHeader>
+            <Table.Cell>3 months to 1 year</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.CellHeader>Focus Area:</Table.CellHeader>
+            <Table.Cell>Front-End</Table.Cell>
+          </Table.Row>
+        </Table>
+        <ThumbDownButton />
+        <Link to="/projects">
+          <ThumbUpButton />
+        </Link>
+        <ProfileImage />
+      </WhiteBackground>
     </TixnerLayout>
   );
 };
