605900ea06a54664cd47db002289ab3420e9352e
Jon-Paul Glencross <jon-paul.glencross@uk.ibm.com>
2019-03-15 15:08:24 +0000

    Re-add needed components

--- client/components/atoms/index.js
+++ client/components/atoms/index.js
@@ -1,5 +1,2 @@
-export { default as HintText } from "./HintText";
-export { default as Link } from "./Link";
 export { default as ContentGroup } from "./ContentGroup";
 export { default as Main } from "./Main";
-export { default as RandomNumber } from "./RandomNumber";
